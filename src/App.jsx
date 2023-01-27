4e234dded366d5e66df1b650839483653c6870c3
George Kelly <george.kelly@ukfast.co.uk>
2023-01-27 11:34:56 +0000

    Dr Bobert

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import './App.css'
 
-const HatsuneNiku = props => <div className="border-2 border-yellow-300 rounded p-2">
-  <p className="font-chakra text-yellow-300">Only 30% synthetic meat at</p>
-  <h3 className="text-white text-5xl text-center animate-[glow_1s_ease-in-out_infinite_alternate]">ハツネ肉</h3>
+const HatsuneNiku = props => <div>
+  <div className="border-2 border-yellow-300 rounded p-2 max-height-full">
+    <p className="font-chakra text-yellow-300">Only 30% synthetic meat at</p>
+    <h3 className="text-white text-5xl text-center animate-[glow_1s_ease-in-out_infinite_alternate]">ハツネ肉</h3>
+  </div>
+</div>
+
+
+const DrBobert = props => <div className="text-white font-chakra max-w-[350px]">
+  <div className="mb-2">
+    <h3 className="text-4xl">No Insurance?</h3>
+    <h3 className="text-4xl">No Money?</h3>
+    <h3 className="text-4xl">No Problem!</h3>
+  </div>
+  <div className="mb-3">
+    <p className='text-2xl text-justify'>"Dr" Bobert will see you now, for a low interest loan!</p>
+  </div>
+  <p className='font-mono text-xs text-justify'>10000% APR. Failure to repay loan will result in organ reposession. Failure to provide organs will result in blood reposession.</p>
 </div>
 
 const Scroller = props => <div className={`w-full overflow-hidden ${props.className}`}>
@@ -18,6 +33,7 @@ function App() {
     <div className="min-h-screen bg-black pt-8">
       <Scroller>
         <HatsuneNiku />
+        <DrBobert />
       </Scroller>
     </div>
   )
