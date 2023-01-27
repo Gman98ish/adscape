6f024402b42cdbdf675bde38977f71a547449c6f
George Kelly <george.kelly@ukfast.co.uk>
2023-01-27 13:18:29 +0000

    WIP: responsiveness tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-const HatsuneNiku = props => <div>
+const HatsuneNiku = props => <div className='min-w-[250px]'>
   <div className="border-2 border-yellow-300 rounded p-2 max-height-full">
     <p className="font-chakra text-yellow-300">Only 30% synthetic meat at</p>
     <h3 className="text-white text-5xl text-center animate-[glow_1s_ease-in-out_infinite_alternate]">ハツネ肉</h3>
@@ -8,7 +8,7 @@ const HatsuneNiku = props => <div>
 </div>
 
 
-const DrBobert = props => <div className="text-white font-chakra max-w-[350px]">
+const DrBobert = props => <div className="text-white font-chakra max-w-[350px] w-screen md:w-auto mx-4">
   <div className="mb-2">
     <h3 className="text-4xl">No Insurance?</h3>
     <h3 className="text-4xl">No Money?</h3>
@@ -21,8 +21,8 @@ const DrBobert = props => <div className="text-white font-chakra max-w-[350px]">
 </div>
 
 const Scroller = props => <div className={`w-full overflow-hidden ${props.className}`}>
-  <div className="w-full flex animate-[scroll_30s_linear_infinite]">
-    {[1,2].map(i => <div key={i} className="pr-20 box-border min-w-full flex justify-between">
+  <div className="flex animate-[scroll_10s_linear_infinite] w-fit md:w-full" style={{transformBox: 'content-box'}}>
+    {[1,2].map(i => <div key={i} className="md:min-w-full min-w-min flex justify-between">
       {props.children}
     </div>)}
   </div>
