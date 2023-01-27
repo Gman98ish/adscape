088db667e179ec30f475b6300e145dba2e3b433b
George Kelly <george.kelly@ukfast.co.uk>
2023-01-27 10:31:04 +0000

    Made horrible glowing text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 const HatsuneNiku = props => <div className="border-2 border-yellow-300 rounded p-2">
   <p className="font-chakra text-yellow-300">Only 30% synthetic meat at</p>
-  <h3 className="text-yellow-400 text-5xl text-center">ハツネ肉</h3>
+  <h3 className="text-white text-5xl text-center animate-[glow_1s_ease-in-out_infinite_alternate]">ハツネ肉</h3>
 </div>
 
 const Scroller = props => <div className={`w-full overflow-hidden ${props.className}`}>
