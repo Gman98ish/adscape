703956922c5aa63d817568115196617b6b733ed8
George Kelly <george.kelly@ukfast.co.uk>
2023-01-29 22:06:12 +0000

    Increased spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import skullUrl from './assets/skull.png'
 
-const HatsuneNiku = props => <div className='min-w-[250px] mx-4 '>
+const HatsuneNiku = props => <div className='min-w-[250px] mx-12 '>
   <div className="border-2 border-yellow-300 rounded p-2 max-height-full">
     <p className="font-chakra text-yellow-300 text-center">Only 30% synthetic meat at</p>
     <h3 className="text-white text-5xl text-center animate-[glow_1s_ease-in-out_infinite_alternate]">ハツネ肉</h3>
   </div>
 </div>
 
-const Coffee = props => <div className='mx-4 text-cyan-500 font-chakra animate-bounce pt-24'>
+const Coffee = props => <div className='mx-12 text-cyan-500 font-chakra animate-bounce pt-24'>
   <div className="flex items-center">
     <svg className='fill-cyan-500' width="50px" height="50px" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
       <path d="M1 20V18H17V20H1M2 3H19V4H20V10H19V11H16V14H15V15H14V16H4V15H3V14H2V3M16 5V9H18V5H16M4 5V13H5V14H13V13H14V5H4Z" />
@@ -35,7 +35,7 @@ const Coffee = props => <div className='mx-4 text-cyan-500 font-chakra animate-b
 </div>
 
 
-const DrBobert = props => <div className="text-white font-chakra max-w-[350px] w-screen md:w-auto mx-4">
+const DrBobert = props => <div className="text-white font-chakra max-w-[350px] w-screen md:w-auto mx-12">
   <div className="mb-2">
     <h3 className="text-4xl">No Insurance?</h3>
     <h3 className="text-4xl">No Money?</h3>
@@ -55,7 +55,7 @@ const Scroller = props => <div className={`w-full overflow-hidden ${props.classN
   </div>
 </div>
 
-const Cash4Bones = props => <div className='mx-4'>
+const Cash4Bones = props => <div className='mx-12 min-w-[250px]'>
   <div className="relative">
     <div className="z-10 relative">
       <img src={skullUrl} alt="" height="200px" width="200px" className='absolute z-0 top-2 left-8 animate-ping' />
